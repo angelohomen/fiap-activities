ea376ee751a596c7016d432f09bf65621e324ab8
Angelo Homen <78157758+angelohomen@users.noreply.github.com>
2024-09-30 18:14:34 -0300

    Removing frontend files

--- TC1/Code/frontend/main.py
+++
@@ -1,3 +0,0 @@
-from src.repositories.embrapa_api import EmbrapaAPI
-from src.models.enum_embrapa_api import EnumEmbrapaAPI
-
--- TC1/Code/frontend/src/models/enum_embrapa_api.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class EnumEmbrapaAPI(Enum):
-    PRODUCAO: int = 1
-    PROCESSAMENTO: int = 2
-    IMPORTACAO: int = 3
-    EXPORTACAO: int = 4
-    COMERCIALIZACAO: int = 5
